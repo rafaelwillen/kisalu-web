aaec33644ff4eaa3274ef140417ffef789b16900
Rafael Willen <rafaelpadre20@gmail.com>
2023-04-14 17:30:29 +0100

    Add the home page categories mock

---
+++ src/mock/category.ts
@@ -0,0 +1,62 @@
+export const homeBrowseByCategory = [
+  {
+    id: 1,
+    name: "Web Development",
+    field: "Software Development",
+    freelancersCount: 2000,
+  },
+  {
+    id: 2,
+    name: "Mobile Development",
+    field: "Software Development",
+    freelancersCount: 1500,
+  },
+  {
+    id: 3,
+    name: "Graphic Design",
+    field: "Design",
+    freelancersCount: 1200,
+  },
+  {
+    id: 4,
+    name: "Copywriting",
+    field: "Writing",
+    freelancersCount: 1000,
+  },
+  {
+    id: 5,
+    name: "Content Creation",
+    field: "Writing",
+    freelancersCount: 800,
+  },
+  {
+    id: 6,
+    name: "Video Editing",
+    field: "Media Production",
+    freelancersCount: 700,
+  },
+  {
+    id: 7,
+    name: "Social Media Management",
+    field: "Marketing",
+    freelancersCount: 600,
+  },
+  {
+    id: 8,
+    name: "Photography",
+    field: "Media Production",
+    freelancersCount: 500,
+  },
+  {
+    id: 9,
+    name: "Translation",
+    field: "Language Services",
+    freelancersCount: 400,
+  },
+  {
+    id: 10,
+    name: "Data Analysis",
+    field: "Business Intelligence",
+    freelancersCount: 300,
+  },
+];
