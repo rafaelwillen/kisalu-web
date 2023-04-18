bfd19e0b603ced0fb79e62b593ec04c0fd79298d
Rafael Willen <rafaelpadre20@gmail.com>
2023-04-18 17:02:13 +0100

    Implement the navbar

--- src/components/pages/common/Navbar.tsx
+++ src/components/pages/common/Navbar.tsx
@@ -8,22 +8,35 @@ export default function Navbar() {
     <nav className="py-5 border-b border-b-white border-opacity-20">
       <Container>
         <ul className="flex justify-between items-center">
-          <li className="flex items-center gap-2 text-2xl font-bold">
+          <li className="flex items-center gap-2 text-2xl font-bold pr-7 border-r border-white/20">
             <Image
               src="/kisalu-logo.png"
               height={40}
               width={40}
               alt="Kisalu Logo"
             />
-            <span>Kisalu</span>
+            <span className="select-none">Kisalu</span>
           </li>
-          <li className="flex items-center gap-7">
+          <li className="hidden lg:flex gap-7 items-center  text-white text-sm font-regular">
+            {/* TODO: Replace with actual link */}
+            <Link href="/">Categorias</Link>
             {/* TODO: Replace with an actual link */}
-            <Link href="#" className="text-white text-sm">
+            <Link href="/">Tornar um prestador</Link>
+          </li>
+          <li className="flex items-center gap-7 pl-7 border-l border-white/20 py-1">
+            {/* TODO: Replace with an actual link */}
+            <Link href="/" className="text-white text-sm hidden lg:inline">
+              Criar Conta
+            </Link>
+            {/* TODO: Replace with an actual link */}
+            <Link
+              href="/"
+              className="text-white text-sm lg:bg-white lg:text-text-200 lg:px-7 lg:py-1 lg:rounded lg:hover:bg-neutral-300 duration-300"
+            >
               Entrar
             </Link>
             {/* TODO: Implement the mobile menu */}
-            <button>
+            <button className="lg:hidden">
               <List size={40} color="#fff" />
             </button>
           </li>
