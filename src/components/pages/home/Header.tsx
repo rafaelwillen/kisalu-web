bd2540dc6234d4320f89032f9e7debf6edca4ad7
Rafael Willen <rafaelpadre20@gmail.com>
2023-04-19 10:37:10 +0100

    Style the header for desktop

--- src/components/pages/home/Header.tsx
+++ src/components/pages/home/Header.tsx
@@ -7,25 +7,54 @@ export default function Header() {
     <header className="relative header-bg text-white pb-16">
       <Navbar />
       <Container small>
-        <h1 className="mt-24 mb-10 text-3xl font-bold ">
-          Contrate os melhores prestadores para qualquer trabalho, online
-        </h1>
-        {/* Probably will replace this with react hook form or just keep with the default form */}
-        <form className="flex flex-col gap-2 ">
-          <div className="relative bg-white rounded flex p-2 items-center gap-2">
-            <MagnifyingGlass size={20} color="#000" />
+        <div className="lg:hidden block">
+          <h1 className="mt-24 mb-10 text-3xl font-bold ">
+            Contrate os melhores prestadores para qualquer trabalho, online
+          </h1>
+          {/* Probably will replace this with react hook form or just keep with the default form */}
+          <form className="flex flex-col gap-2 ">
+            <div className="relative bg-white rounded flex p-2 items-center gap-2">
+              <MagnifyingGlass size={20} color="#000" />
+              {/* TODO: Refactor this to be a component */}
+              <input
+                type="search"
+                className="flex-1 py-2 text-text-200 font-regular"
+                placeholder="Está a procura de um serviço?"
+              />
+            </div>
             {/* TODO: Refactor this to be a component */}
-            <input
-              type="text"
-              className="flex-1 py-2 text-text-200 font-regular"
-              placeholder="Está a procura de um serviço?"
-            />
-          </div>
-          {/* TODO: Refactor this to be a component */}
-          <button className="bg-primary-600 font-bold text-sm py-4 rounded duration-300 hover:bg-primary-500">
-            Pesquisar
-          </button>
-        </form>
+            <button className="bg-primary-600 font-bold text-sm py-4 rounded duration-300 hover:bg-primary-500">
+              Pesquisar
+            </button>
+          </form>
+        </div>
+        <div className="hidden lg:block pt-52 pb-64 max-w-3xl">
+          <h1 className="mb-16 text-4xl font-bold">
+            Contrate os melhores prestadores para qualquer trabalho, online
+          </h1>
+          <form className="flex items-center text-text-200 bg-white pl-7 pr-2 py-2 rounded gap-7">
+            <div className="flex items-center flex-1 border-r pr-2 border-neutral-200 gap-2">
+              <MagnifyingGlass size={20} />
+              <input
+                type="search"
+                className="flex-1 placeholder:font-regular text-base py-1 px-1 outline-none border-b border-neutral-200/0 focus:border-neutral-500 duration-300"
+                placeholder="O que está procurando?"
+              />
+            </div>
+            {/* TODO: Add radix select */}
+            <select name="" id="">
+              <option value="1">Selecione a categoria de pesquisa</option>
+              <option value="2">Lorem ipsum dolor sit.</option>
+              <option value="3">Lorem ipsum dolor sit.</option>
+              <option value="4">Lorem ipsum dolor sit.</option>
+              <option value="5">Lorem ipsum dolor sit.</option>
+            </select>
+            {/* TODO: Refactor this to be a component */}
+            <button className="bg-primary-600 font-bold text-sm py-4 rounded duration-300 hover:bg-primary-500 px-9 text-white">
+              Pesquisar
+            </button>
+          </form>
+        </div>
       </Container>
     </header>
   );
