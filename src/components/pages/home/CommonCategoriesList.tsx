10094b39c51602b900fc5c778fc5414e5ca075dd
Rafael Willen <rafaelpadre20@gmail.com>
2023-04-19 11:39:41 +0100

    Remove overflow for desktop

--- src/components/pages/home/CommonCategoriesList.tsx
+++ src/components/pages/home/CommonCategoriesList.tsx
@@ -16,7 +16,7 @@ export default function CommonCategoriesList() {
           <ArrowRight className="group-hover:translate-x-2 duration-300" />
         </Link>
       </div>
-      <article className="mt-5 flex overflow-auto gap-2 lg:grid grid-cols-4 lg:gap-7 lg:mt-0 lg:py-4">
+      <article className="mt-5 flex overflow-auto gap-2 lg:grid grid-cols-4 lg:gap-7 lg:mt-0 lg:py-4 lg:overflow-hidden">
         {homeBrowseByCategory.map((category) => (
           <CommonCategoryCard {...category} key={category.id} />
         ))}
