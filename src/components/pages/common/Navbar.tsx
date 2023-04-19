3f45c9deb9d58dc99aaae85668e7f0d00a126eba
Rafael Willen <52548623+rafaelwillen@users.noreply.github.com>
2023-04-19 22:07:24 +0100

    22 create animated underline text (#25)
    * Add angolan bg image
    
    * Add animated link hover
    
    * Replace the placeholder

--- src/components/pages/common/Navbar.tsx
+++ src/components/pages/common/Navbar.tsx
@@ -19,19 +19,26 @@ export default function Navbar() {
           </li>
           <li className="hidden lg:flex gap-7 items-center  text-white text-sm font-regular">
             {/* TODO: Replace with actual link */}
-            <Link href="/">Categorias</Link>
+            <Link href="/" className="animated-underline">
+              Categorias
+            </Link>
             {/* TODO: Replace with an actual link */}
-            <Link href="/">Tornar um prestador</Link>
+            <Link href="/" className="animated-underline">
+              Tornar um prestador
+            </Link>
           </li>
           <li className="flex items-center gap-7 pl-7 border-l border-white/20 py-1">
             {/* TODO: Replace with an actual link */}
-            <Link href="/" className="text-white text-sm hidden lg:inline">
+            <Link
+              href="/"
+              className="text-white text-sm hidden lg:inline animated-underline"
+            >
               Criar Conta
             </Link>
             {/* TODO: Replace with an actual link */}
             <Link
               href="/"
-              className="text-white text-sm lg:bg-white lg:text-text-200 lg:px-7 lg:py-1 lg:rounded lg:hover:bg-neutral-300 duration-300"
+              className="text-white text-sm lg:bg-white lg:text-text-200 lg:px-7 lg:py-1 lg:rounded lg:hover:bg-neutral-300 duration-300 animated-underline"
             >
               Entrar
             </Link>
