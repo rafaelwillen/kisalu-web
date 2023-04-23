b2c5350dfff897b3a156fdcecb77447d664121ff
Rafael Willen <rafaelpadre20@gmail.com>
2023-04-23 15:28:11 +0100

    Add mock data for the categories page

---
+++ src/pages/categories/index.tsx
@@ -0,0 +1,23 @@
+import MainLayout from "@/components/layouts/MainLayout";
+import CategoryCard from "@/components/pages/categories/CategoryCard";
+import { Container } from "@/components/pages/common";
+import { categoriesPageResult } from "@/mock/category";
+import { NextPageWithLayout } from "../_app";
+
+const CategoriesPage: NextPageWithLayout = () => {
+  return (
+    <Container>
+      <h1>Categorias Disponíveis</h1>
+      <section>
+        {categoriesPageResult.map((category) => (
+          <CategoryCard key={category.name} />
+        ))}
+      </section>
+      {/* TODO: Add the pagination here */}
+    </Container>
+  );
+};
+
+CategoriesPage.getLayout = (page) => <MainLayout>{page}</MainLayout>;
+
+export default CategoriesPage;
