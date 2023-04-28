be31b04cc79f5d3c0457ff693f574e97f772ea03
Rafael Willen <rafaelpadre20@gmail.com>
2023-04-28 12:42:03 +0100

    Style the service product banner

--- src/pages/service/product/[id].tsx
+++ src/pages/service/product/[id].tsx
@@ -15,7 +15,7 @@ import Link from "next/link";
 
 const ServicePage: NextPageWithLayout = () => {
   return (
-    <main className="py-10 lg:py-32">
+    <main className="py-10 lg:pt-0">
       <Container small>
         <section className="space-x-2 my-5 hidden lg:block text-text-200/60">
           <Link className="animated-underline dark" href={Routes.home}>
