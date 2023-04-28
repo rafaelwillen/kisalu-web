0381ed70670373a7b01ca7218fbfd1556ecfb47d
Rafael Willen <rafaelpadre20@gmail.com>
2023-04-28 12:14:34 +0100

    Add ref
    Reason because next/link with legacy behavior, is being a baby and complains about ref not being passed

--- src/components/buttons/PrimaryButton.tsx
+++ src/components/buttons/PrimaryButton.tsx
@@ -1,28 +1,29 @@
 import classNames from "classnames";
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, forwardRef } from "react";
 import { PrimaryButtonProps } from "./types";
 
-export default function PrimaryButton({
-  variant = "solid",
-  children,
-  fitContent = false,
-  ...props
-}: PropsWithChildren<PrimaryButtonProps>) {
-  return (
-    <button
-      {...props}
-      className={classNames(
-        "mt-5 py-2 flex gap-2 justify-center rounded duration-300 ease-in-out disabled:opacity-30 items-center",
-        variant === "solid" &&
-          "bg-primary-600 text-white hover:bg-primary-400 active:bg-primary-300",
-        variant === "outline" &&
-          "border border-primary-600 text-primary-600 hover:bg-primary-600 hover:text-white",
-        variant === "text" &&
-          "text-primary-600 hover:bg-primary-50 font-bold hover:text-primary-700",
-        fitContent ? "w-fit px-4" : "w-full"
-      )}
-    >
-      {children}
-    </button>
-  );
-}
+
+const PrimaryButton = forwardRef<
+  HTMLButtonElement,
+  PropsWithChildren<PrimaryButtonProps>
+>(({ variant = "solid", children, fitContent = false, ...props }, ref) => (
+  <button
+    ref={ref}
+    {...props}
+    className={classNames(
+      "mt-5 py-2 flex gap-2 justify-center rounded duration-300 ease-in-out disabled:opacity-30 items-center",
+      variant === "solid" &&
+        "bg-primary-600 text-white hover:bg-primary-400 active:bg-primary-300",
+      variant === "outline" &&
+        "border border-primary-600 text-primary-600 hover:bg-primary-600 hover:text-white",
+      variant === "text" &&
+        "text-primary-600 hover:bg-primary-50 font-bold hover:text-primary-700",
+      fitContent ? "w-fit px-4" : "w-full"
+    )}
+  >
+    {children}
+  </button>
+));
+
+export default PrimaryButton;
+
