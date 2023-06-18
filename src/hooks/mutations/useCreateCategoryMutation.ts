343f0742a40e7d9e0802f3d9faf2af47773ef2e1
Rafael Willen <rafaelpadre20@gmail.com>
2023-06-18 01:06:51 +0100

    Refactor the category mutation

--- src/hooks/mutations/useCreateCategoryMutation.ts
+++ src/hooks/mutations/useCreateCategoryMutation.ts
@@ -1,10 +1,12 @@
 import { createCategory } from "@/api/category";
 import { CreateCategoryRequestBody } from "@/api/types/request";
 import { uploadCategoryImage } from "@/api/upload";
+import { useAuth } from "@/context/AuthContext";
 import { useMutation } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 
 export default function useCreateCategoryMutation() {
+  const { token } = useAuth();
   const uploadFileMutation = useMutation(
     (file: File) => uploadCategoryImage(file),
     {
@@ -14,7 +16,7 @@ export default function useCreateCategoryMutation() {
     }
   );
   const createCategoryMutation = useMutation(
-    (category: CreateCategoryRequestBody) => createCategory(category),
+    (category: CreateCategoryRequestBody) => createCategory(category, token),
     {
       onError: (error) => {
         toast.error((error as Error).message);
