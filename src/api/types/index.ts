62e4ef5d029b45cf679355e0c30e7a773d3484b3
Rafael Willen <rafaelpadre20@gmail.com>
2023-06-28 12:23:29 +0100

    Remove duplicates

--- src/api/types/index.ts
+++ src/api/types/index.ts
@@ -19,14 +19,6 @@ export type CategoryCreator = {
   gender: Role;
 };
 
-export type CategoryCreator = {
-  id: string;
-  firstName: string;
-  lastName: string;
-  avatarImageURL: string;
-  gender: Role;
-};
-
 export type Role = "Administrator" | "Provider" | "Client";
 export type Gender = "Male" | "Female";
 
