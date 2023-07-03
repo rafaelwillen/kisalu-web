76f728479a883ef8b3c4f3f290120e5e4ea286b7
Rafael Willen <rafaelpadre20@gmail.com>
2023-07-03 16:29:01 +0100

    Add icons for each slug

--- src/app/(home)/_components/CommonCategoryCard.tsx
+++ src/app/(home)/_components/CommonCategoryCard.tsx
@@ -1,8 +1,8 @@
 import { GetMostPopularCategoriesResponseBody } from "@/api/types/response";
 import { Routes } from "@/utils/constants/routes";
 import { formatToNumber } from "@/utils/intl";
-import { BoxSelect } from "lucide-react";
 import Link from "next/link";
+import PopularCategoryIconSelector from "./PopularCategoryIconSelector";
 
 type Props = {
   category: GetMostPopularCategoriesResponseBody[number];
@@ -16,10 +16,7 @@ export default function CommonCategoryCard({ category }: Props) {
       className="px-7 py-10 space-y-7 flex-1 border border-neutral-200 lg:border-neutral-50 lg:shadow-lg hover:bg-neutral-200/40 duration-300 cursor-pointer"
     >
       <div className="relative  inline-block">
-        <BoxSelect
-          size={40}
-          className="text-primary-600 relative category-bg-icon "
-        />
+        <PopularCategoryIconSelector slug={slug} />
         <div className="absolute -z-10 -right-3 -bottom-2 bg-primary-100 w-10 h-10 rounded-full" />
       </div>
       <div className="space-y-1">
