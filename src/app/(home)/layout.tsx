cbdc51f6db4de129c68932e3fe90bbdff9a22533
Rafael Willen <52548623+rafaelwillen@users.noreply.github.com>
2023-07-05 13:03:55 +0100

    76 change the hero on the home page (#84)
    * Replace the css bg image to next image
    
    * Replace hero image and made it darker

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,14 +1,24 @@
+import { HeroImage } from "@/assets/images";
 import ScrollToTopButton from "@/components/buttons/ScrollToTopButton";
 import Container from "@/components/common/Container";
 import Footer from "@/components/common/Footer";
 import Navbar from "@/components/common/Navbar";
+import Image from "next/image";
 import { PropsWithChildren } from "react";
 import Hero from "./_components/Hero";
 
 export default function HomePageLayout({ children }: PropsWithChildren) {
   return (
     <>
-      <header className="relative text-white pb-16 header-bg">
+      <header className="relative text-white pb-16 h-screen shadow-2xl">
+        <Image
+          src={HeroImage}
+          className="h-full brightness-[.4] -z-10 object-cover object-center"
+          fill
+          alt=""
+          placeholder="blur"
+          priority
+        />
         <Navbar />
         <Hero />
       </header>
