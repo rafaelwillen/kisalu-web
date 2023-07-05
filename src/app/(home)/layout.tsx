62c8a2b27fd9d5a81092ad89479e993c357b1b4e
Rafael Willen <rafaelpadre20@gmail.com>
2023-07-05 12:52:28 +0100

    Replace the css bg image to next image

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,14 +1,22 @@
+import { HeroImage } from "@/assets/images";
 import ScrollToTopButton from "@/components/buttons/ScrollToTopButton";
 import Container from "@/components/common/Container";
 import Footer from "@/components/common/Footer";
 import Navbar from "@/components/common/Navbar";
+import Image from "next/image";
 import { PropsWithChildren } from "react";
 import Hero from "./_components/Hero";
 
 export default function HomePageLayout({ children }: PropsWithChildren) {
   return (
     <>
-      <header className="relative text-white pb-16 header-bg">
+      <header className="relative text-white pb-16 h-screen">
+        <Image
+          src={HeroImage}
+          className="h-full brightness-75 -z-10 object-cover object-center"
+          fill
+          alt=""
+        />
         <Navbar />
         <Hero />
       </header>
--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,5 +1,7 @@
 import BusinessImage from "./business.jpg";
+import HeroImage from "./hero-background.jpg";
 import KisaluLogoDark from "./logo-dark.png";
 import KisaluLogo from "./logo.png";
 
-export { KisaluLogo, KisaluLogoDark, BusinessImage };
+export { BusinessImage, HeroImage, KisaluLogo, KisaluLogoDark };
+
