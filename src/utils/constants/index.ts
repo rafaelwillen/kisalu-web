98d183df5e091b3c74a85157a2f01bfdfeb77620
Rafael Willen <rafaelpadre20@gmail.com>
2023-07-06 14:53:17 +0100

    Add Next JS API endpoint for user authentication

--- src/utils/constants/index.ts
+++ src/utils/constants/index.ts
@@ -13,3 +13,5 @@ export const DEFAULT_USER_AVATAR_API_URL =
   "https://api.dicebear.com/6.x/notionists/png";
 
 export const ADULT_DATE_OF_BIRTH = subYears(new Date(), 17);
+
+export const COOKIES_EXPIRATION_TIME = 60 * 60 * 24 * 1; // 1 day
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,11 @@
+import { COOKIES_EXPIRATION_TIME } from "./constants";
+
 export function decodeObjectURL(objectURL: string) {
   const path = new URL(objectURL).pathname;
   const decodedPath = decodeURIComponent(path);
   return decodedPath;
+}
+
+export function generateJWTCookie(token: string) {
+  return `token=${token}; Path=/; HttpOnly; Secure; Max-Age=${COOKIES_EXPIRATION_TIME}; SameSite=Strict`;
 }
