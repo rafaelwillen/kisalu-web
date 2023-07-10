d4fcfa022703b86f0396efe908efbb9d4dff0b1a
Rafael Willen <rafaelpadre20@gmail.com>
2023-07-10 22:26:20 +0100

    Add the page setup

---
+++ src/app/provider-dashboard/profile/page.tsx
@@ -0,0 +1,9 @@
+import PageHeader from "../_components/PageHeader";
+
+export default function ProviderProfilePage() {
+  return (
+    <>
+      <PageHeader pageTitle="O meu Perfil" pageDescription="" />
+    </>
+  );
+}
